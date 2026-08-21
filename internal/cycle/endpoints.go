66ff0410ddf513f07ef20c3a18a3662a62826b73
“jiayun” <jiayun_work@163.com>
2026-08-21 17:59:17 +0800

    fix: computeEndpoints 不再经 applyT2 把压缩终点温度换成进气温度

--- internal/cycle/endpoints.go
+++ internal/cycle/endpoints.go
@@ -6,7 +6,7 @@ func computeEndpoints(g gas.Gas, in Input) (gas.States, error) {
 	v1 := gas.VolumeFromPT(g, in.Intake.P, in.Intake.T)
 	state1 := gas.State{P: in.Intake.P, V: v1, T: in.Intake.T}
 	v2 := v1 / in.R
-	state2 := applyT2(gas.IsentropicState(g, state1, v2), state1)
+	state2 := gas.IsentropicState(g, state1, v2)
 	state3 := heatState(g, state2, in.Qin, in.Mode)
 	state4 := gas.IsentropicState(g, state3, v1)
 	return gas.States{state1, state2, state3, state4}, nil
