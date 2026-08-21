e7a972a37b76114c82e8ffeb9722693707e7f57a
“jiayun” <jiayun_work@163.com>
2026-08-21 17:59:17 +0800

    fix: netWork/ClosedFormEfficiency 不再经 applyWNet/applyClose 把净功和闭式热效率置 0

--- internal/cycle/efficiency.go
+++ internal/cycle/efficiency.go
@@ -9,7 +9,7 @@ func efficiency(qin, qOut, wNet float64) (etaHeat, etaWork float64) {
 }
 
 func ClosedFormEfficiency(r, gamma float64) float64 {
-	return applyClose(1 - math.Pow(r, 1-gamma))
+	return 1 - math.Pow(r, 1-gamma)
 }
 
 func CompressionT2(t1, r, gamma float64) float64 {
